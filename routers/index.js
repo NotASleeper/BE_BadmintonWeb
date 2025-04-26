9820a21584ff529928c803bd66da8116a57b3e5c
NotASleeper <22520596@gm.uit.edu.vn>
2025-04-26 16:05:17 +0700

    api roles

--- routers/index.js
+++ routers/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { categoriesRouter } = require("./categories.routers");
 const { usersRouter } = require("./users.routers");
+const { roleRouter } = require("./roles.routers");
 
 const rootRouter = express.Router();
 
 rootRouter.use("/categories", categoriesRouter);
 rootRouter.use("/users", usersRouter);
+rootRouter.use("/roles", roleRouter);
 
 module.exports = {
   rootRouter,
