e148190e7aad50a611e434497b8b10d73e735ef5
NotASleeper <22520596@gm.uit.edu.vn>
2025-06-04 00:08:56 +0700

    update top 5 product which are bought the most and include model

--- controllers/products.controllers.js
+++ controllers/products.controllers.js
@@ -1,5 +1,11 @@
-const { Op } = require("sequelize");
-const { Products, Imagesproduct, Categories } = require("../models");
+const { Op, fn, col, literal } = require("sequelize");
+const {
+  Products,
+  Imagesproduct,
+  Categories,
+  Orders,
+  Ordersdetail,
+} = require("../models");
 
 const createProducts = async (req, res) => {
   try {
@@ -41,7 +47,18 @@ const getAllProducts = async (req, res) => {
       });
       res.status(200).send(productslist);
     } else {
-      const productslist = await Products.findAll();
+      const productslist = await Products.findAll({
+        include: [
+          {
+            model: Categories,
+            attributes: ["name"],
+          },
+          {
+            model: Imagesproduct,
+            attributes: ["url"],
+          },
+        ],
+      });
       res.status(200).send(productslist);
     }
   } catch (error) {
@@ -105,10 +122,48 @@ const deleteProducts = async (req, res) => {
     res.status(500).send(error);
   }
 };
+
+const getTop5ProductsByMonth = async (req, res) => {
+  const { month, year } = req.query;
+  try {
+    const startDate = new Date(year, month - 1, 1);
+    const endDate = new Date(year, month, 1);
+
+    const topProducts = await Ordersdetail.findAll({
+      attributes: ["productid", [fn("SUM", col("quantity")), "totalSold"]],
+      include: [
+        {
+          model: Orders,
+          attributes: [],
+          where: {
+            createdAt: {
+              [Op.gte]: startDate,
+              [Op.lt]: endDate,
+            },
+            status: 2, // chỉ tính đơn đã hoàn thành (nếu có trường status)
+          },
+        },
+        {
+          model: Products,
+          attributes: ["id", "name", "price"],
+        },
+      ],
+      group: ["productid", "Product.id"],
+      order: [[literal("totalSold"), "DESC"]],
+      limit: 5,
+    });
+
+    res.status(200).send(topProducts);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
 module.exports = {
   createProducts,
   getAllProducts,
   getDetailProducts,
   updateProducts,
   deleteProducts,
+  getTop5ProductsByMonth,
 };
