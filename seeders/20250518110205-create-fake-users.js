02176db19bfba06155396b3733828f202a1c7269
NotASleeper <22520596@gm.uit.edu.vn>
2025-06-12 16:01:22 +0700

    create seeder for product and image

--- seeders/20250518110205-create-fake-users.js
+++ seeders/20250518110205-create-fake-users.js
@@ -88,5 +88,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    await queryInterface.bulkDelete("users", null, {});
   },
 };
--- seeders/20250518110221-create-fake-categories.js
+++ seeders/20250518110221-create-fake-categories.js
@@ -68,5 +68,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    await queryInterface.bulkDelete("categories", null, {});
   },
 };
